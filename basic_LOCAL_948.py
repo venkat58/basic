5a0ed763b7c03df1fc70b5ef5cd77a907450c155
Raju_Infore <raju@inforesoftware.com>
2020-03-22 12:08:32 +0530

    v1.5

--- basic_LOCAL_948.py
+++
@@ -1,7 +0,0 @@
-import json  
-  
-person = '{"Name": "Andrew","City":"English", "Number":90014, "Age": 23,"Subject": ["Data Structure","Computer Graphics", "Discrete mathematics"]}'  
-  
-per_dict = json.loads(person)  
-  
-print(json.dumps(per_dict, indent = 5, sort_keys= True))  
--- basic_REMOTE_948.py
+++
@@ -1,8 +0,0 @@
-import json
-
-student= {
-    "name": 'venkat',
-    'age': 34
-}
-s = json.load(student)
-print('name {student.name}'.format(s.name))
