759102289ca3d25309cb887067068ce6eb1c6eb7
Raju_Infore <raju@inforesoftware.com>
2020-03-23 09:52:04 +0530

    v1.6

--- basic.py
+++ basic.py
@@ -3,7 +3,8 @@ import json
 student= {
     "name": 'venkat',
     'age': 34,
-    'sex':'male'
+    'sex':'male',
+    'address':'ylm'
 }
 s = json.load(student)
 print('name {student.name}'.format(s.name))
