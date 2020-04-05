b692d7395e7180cc841da8e567a4cf8535a784a0
Infore <infore@192.168.1.5>
2020-04-05 22:27:15 +0530

    g

---
+++ coroutines.py
@@ -0,0 +1,13 @@
+import calendar  #module of python to provide useful fucntions related to calendar
+import datetime # module of python to get the date and time 
+
+
+def findDay(date):
+    born = datetime.datetime.strptime(date, '%d %m %Y').weekday() #this statement returns an integer corresponding to the day of the week
+    print(born)
+    return (calendar.day_name[born]) #this statement returns the corresponding day name to the integer generated in the previous statement
+
+
+# Driver program 
+date = '3 02 2019' #this is the input date
+print(findDay(date))  # h
