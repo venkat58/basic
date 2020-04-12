fd579a17efb5c587469928e0b5e25c16525acef4
Raju_Infore <raju@inforesoftware.com>
2020-04-12 11:35:33 +0530

    2.0

--- basic.py
+++
@@ -1,8 +0,0 @@
-import json
-import time
-
-student= '{"name":"venkat","age":34,"sex":"male"}'
-s = json.loads(student)
-print('name {0}'.format(s['name']))
-print(f"function format name {s['name']}")
-print(time.localtime(time.time()))  
